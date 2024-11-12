cf248ef137d04a3ee2aa88f5254b8eecc367ee7a
noa <noa@gmail.com>
2024-11-12 17:41:35 +0200

    second

--- ex5.js
+++ ex5.js
@@ -1,4 +1,4 @@
 for (let i = 0; i < catalog.length; i++) {
     catalog[i]["id"]=100+i;
 }
-console.log(catalog);
+// console.log(catalog);
--- func.js
+++ func.js
@@ -1,8 +1,16 @@
-function f1(){
-    document.querySelector("#res").innerHTML="";
-    let name=document.querySelector("#filter").value;
-    const catalog1=catalog.filter(x=>x.title.includes(name))
-    for (let i = 0; i < catalog1.length; i++) {
-      document.querySelector("#res").innerHTML+=catalog1[i]["title"]+" ";
-    }
+function f1() {
+  document.querySelector("#res").innerHTML = "";
+  let name = document.querySelector("#filter").value;
+  const catalog1 = catalog.filter(x => x.title.includes(name))
+  for (let i = 0; i < catalog1.length; i++) {
+    document.querySelector("#res").innerHTML += catalog1[i]["title"] + " ";
+  }
 }
+
+function f2() {
+
+  const country = catalog.map(x=>x.country).filter((item, index,arr) => !arr.includes(item, index + 1))
+  console.log(country);
+
+}
+
